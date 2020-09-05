76a19f2bf28b68a7ab9a8fb458dc69864dc512be
User-Cool <34473770+User-Cool@users.noreply.github.com>
2020-09-05 16:44:43 +0800

    怎么在创建显示之后立刻调用方法。

---
+++ OnWindowCreated/OnWindowCreated/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OnWindowCreated
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 当窗口渲染完成之后。
+        /// </summary>
+        /// <remarks>
+        /// 在 MainWIndow 下和 ContentRendered 事件进行关联。
+        /// </remarks>
+        internal void OnMainWindowContentRendered(object sender, EventArgs e)
+        {
+
+            /* do something */
+
+        }
+
+
+        #region INotifyPropertyChanged接口实现
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+        #endregion
+    }
+}
